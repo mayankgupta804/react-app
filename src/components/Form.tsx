ff5d6efa8f5c07bf31e71ee6b4b0f95ec9c6af3f
mayankgupta804 <mayank.gupta804@gmail.com>
2024-06-26 17:49:56 +0530

    Create the three form elements

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,12 +3,11 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 const schema = z.object({
-  name: z
+  description: z
     .string()
-    .min(3, { message: "Name must contain at least 3 characters." }),
-  age: z
-    .number({ invalid_type_error: "Age field is required." })
-    .min(18, { message: "Age must be at least 18." }),
+    .min(3, { message: "Description should contain at least 3 characters." }),
+  amount: z.number({ invalid_type_error: "Amount is required." }),
+  category: z.string().min(3, { message: "Category is required." }),
 });
 
 type FormData = z.infer<typeof schema>;
@@ -17,7 +16,7 @@ const Form = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors, isValid },
+    formState: { errors },
   } = useForm<FormData>({ resolver: zodResolver(schema) });
 
   const onSubmit = (data: FieldValues) => {
@@ -27,30 +26,48 @@ const Form = () => {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
-        <label htmlFor="name" className="form-label">
-          Name
+        <label htmlFor="description" className="form-label">
+          Description
         </label>
         <input
-          {...register("name")}
-          id="name"
+          {...register("description")}
+          id="description"
           type="text"
           className="form-control"
         />
-        {errors.name && <p className="text-danger">{errors.name.message}</p>}
+        {errors.description && (
+          <p className="text-danger">{errors.description.message}</p>
+        )}
       </div>
       <div className="mb-3">
-        <label htmlFor="age" className="form-label">
-          Age
+        <label htmlFor="amount" className="form-label">
+          Amount
         </label>
         <input
-          {...register("age", { valueAsNumber: true })}
-          id="age"
+          {...register("amount", { valueAsNumber: true })}
+          id="amount"
           type="number"
           className="form-control"
         />
-        {errors.age && <p className="text-danger">{errors.age.message}</p>}
+        {errors.amount && (
+          <p className="text-danger">{errors.amount.message}</p>
+        )}
       </div>
-      <button disabled={!isValid} className="btn btn-primary" type="submit">
+      <div className="mb-3">
+        <label htmlFor="category" className="form-label">
+          Category
+        </label>
+        <select {...register("category")} className="form-control">
+          <option value=""></option>
+          <option value="Groceries">Groceries</option>
+          <option value="Utilities">Utilities</option>
+          <option value="Entertainment">Entertainment</option>
+        </select>
+        {errors.category && (
+          <p className="text-danger">{errors.category.message}</p>
+        )}
+      </div>
+      <button className="btn btn-primary" type="submit">
         Submit
       </button>
     </form>
