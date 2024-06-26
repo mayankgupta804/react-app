fb698dbafb9de44bba10e2a1829153811993cfbb
mayankgupta804 <mayank.gupta804@gmail.com>
2024-06-26 12:33:56 +0530

    Implement a form + fetch its form values using refs

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import Alert from "./components/Alert";
-import Button from "./components/Button";
-import { useState } from "react";
+import Form from "./components/Form";
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
-
   return (
     <>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
-      )}
-      <Button onClick={() => setAlertVisibility(true)}>My Button</Button>
+      <Form />
     </>
   );
 }
