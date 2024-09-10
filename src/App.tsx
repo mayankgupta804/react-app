3fa25d274fd18f32ff1e5ab77e8762cedac79ead
mayankgupta804 <mayank.gupta804@gmail.com>
2024-09-10 15:42:31 +0530

    Show loader while fetching data from the server

--- src/App.tsx
+++ src/App.tsx
@@ -17,22 +17,27 @@ interface Error {
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [err, setErr] = useState<Error>();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/xusers")
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
       .then((res) => setUsers(res.data))
-      .catch((err) => setErr(err));
+      .catch((err) => setErr(err))
+      .finally(() => setIsLoading(false));
   }, []);
 
   return (
     <>
-      {err ? (
+      {err && (
         <>
           <p className="text-danger">{err.name}</p>
           <p className="text-danger">{err.message}</p>
           <p className="text-danger">{err.code}</p>
         </>
+      )}
+      {isLoading ? (
+        <div className="spinner-border"></div>
       ) : (
         <>
           <p>User details</p>
