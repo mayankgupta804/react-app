fe7281157a45ccffa065cc2626c111cf742f4244
mayankgupta804 <mayank.gupta804@gmail.com>
2024-09-10 15:02:17 +0530

    Learn how to handle HTTP errors

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,41 @@ interface User {
   email: string;
 }
 
+interface Error {
+  name: string;
+  message: string;
+  code: string;
+}
+
 function App() {
   const [users, setUsers] = useState<User[]>([]);
+  const [err, setErr] = useState<Error>();
 
   useEffect(() => {
     axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => setUsers(res.data));
-  });
+      .get<User[]>("https://jsonplaceholder.typicode.com/xusers")
+      .then((res) => setUsers(res.data))
+      .catch((err) => setErr(err));
+  }, []);
 
   return (
     <>
-      <p>User details</p>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      {err ? (
+        <>
+          <p className="text-danger">{err.name}</p>
+          <p className="text-danger">{err.message}</p>
+          <p className="text-danger">{err.code}</p>
+        </>
+      ) : (
+        <>
+          <p>User details</p>
+          <ul>
+            {users.map((user) => (
+              <li key={user.id}>{user.name}</li>
+            ))}
+          </ul>
+        </>
+      )}
     </>
   );
 }
