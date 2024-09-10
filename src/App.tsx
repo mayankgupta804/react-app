fca473bcc723d18dfba0b041b008e274c949d5ac
mayankgupta804 <mayank.gupta804@gmail.com>
2024-09-10 15:26:43 +0530

    Learn how to cancel a request

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { CanceledError } from "axios";
 import { useEffect, useState } from "react";
 
 interface User {
@@ -19,10 +19,19 @@ function App() {
   const [err, setErr] = useState<Error>();
 
   useEffect(() => {
+    const controller = new AbortController();
     axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/xusers")
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
       .then((res) => setUsers(res.data))
-      .catch((err) => setErr(err));
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setErr(err);
+      });
+
+    return () =>
+      controller.abort("Request cancelled because the user moved away");
   }, []);
 
   return (
