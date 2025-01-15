faffe9d71bac645142de78093e04891a1c60c087
mayankgupta804 <mayank.gupta804@gmail.com>
2025-01-15 13:36:51 +0530

    Build a re-usable button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import Alert from "./components/Alert";
 import Button from "./components/Button";
-import { useState } from "react";
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
 
   return (
-    <>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
-      )}
-      <Button onClick={() => setAlertVisibility(true)}>My Button</Button>
-    </>
+    <Button onClick={() => console.log("Hello World")} color="danger">My Button</Button>
   );
 }
 
