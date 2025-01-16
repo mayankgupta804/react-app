98cc742501406c69ee7d514d23e60447e581d9b2
mayankgupta804 <mayank.gupta804@gmail.com>
2025-01-16 14:46:54 +0530

    React updates the state asynchronously

--- src/components/Like.tsx
+++ src/components/Like.tsx
@@ -10,7 +10,16 @@ const Like = ({ onClick }: Props) => {
 
   const toggle = () => {
     onClick();
+    // Lesson: State updates in React happen asynchronously, and
+    // also in a batch, so the update to the state variable will
+    // happen after the rendering is completed. This because
+    // every time we call a "setX" function, React re-render
+    // the component, and this would hamper the overall performance.
+    // That is why, for performance reasons, React updates the state
+    // in a batch, asynchronously.
+    console.log(state);
     setState(!state);
+    console.log(state);
   };
 
   return (
