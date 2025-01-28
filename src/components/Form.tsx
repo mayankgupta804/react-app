85d84ae9c313dafdd8c77c9cf29fa9032542d94c
mayankgupta804 <mayank.gupta804@gmail.com>
2025-01-28 18:13:24 +0530

    Building a form using controlled components

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,32 +1,34 @@
-import { FormEvent, useRef } from "react";
+import { FormEvent, useState } from "react";
 
 const Form = () => {
 
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
-  const person = { name: "", age: 0 }
+  const [person, setPerson] = useState({ name: "", age: "" })
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    if (nameRef.current !== null)
-      person.name = nameRef.current.value
-
-    if (ageRef.current !== null)
-      person.age = parseInt(ageRef.current.value)
-
     console.log(person)
   }
-
 
   return (
     <form onSubmit={handleSubmit}>
       <div className="mb-3">
         <label htmlFor="form-label">Name</label>
-        <input ref={nameRef} id="form-label" type="text" className="form-control" />
+        <input
+          onChange={(event) => setPerson({ ...person, name: event.target.value })}
+          id="form-label"
+          value={person.name}
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">Age</label>
-        <input ref={ageRef} id="age" type="number" className="form-control" />
+        <input
+          onChange={(event) => setPerson({ ...person, age: event.target.value })}
+          id="age"
+          value={person.age}
+          type="number"
+          className="form-control" />
       </div>
       <button className="btn btn-primary" type="submit">Submit</button>
     </form>
