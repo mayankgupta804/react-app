dbb0d6ea4a8f435e705a2d1bd5e5baf26562f299
mayankgupta804 <mayank.gupta804@gmail.com>
2025-02-06 17:44:09 +0530

    Show loader when data is loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { CanceledError } from 'axios';
 import { useEffect, useState } from 'react';
 
 interface User {
@@ -14,19 +14,31 @@ interface Error {
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState<Error>();
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
+    const controller = new AbortController();
+
     axios.
-      get<User[]>("https://jsonplaceholder.typicode.com/usersx").
-      then((res) => setUsers(res.data)).
+      get<User[]>("https://jsonplaceholder.typicode.com/users", { signal: controller.signal }).
+      then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      }).
       catch((err) => {
+        if (err instanceof CanceledError) return;
         setError(err);
-      })
-  }, [])
+        setLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
 
   return (
     <>
       {error && <p>{error.code}</p>}
+      {isLoading &&
+        <div className="spinner-border"></div>}
       <ul>
         {users.map(user => <li key={user.id}>{user.name}</li>)}
       </ul>
