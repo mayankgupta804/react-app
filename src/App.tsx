aa7095ba314df0ddc0e965c57502cd8f431dbd26
mayankgupta804 <mayank.gupta804@gmail.com>
2025-02-07 09:58:35 +0530

    Handle user deletion and addition

--- src/App.tsx
+++ src/App.tsx
@@ -34,13 +34,48 @@ function App() {
     return () => controller.abort();
   }, []);
 
+  const handleDelete = (id: number) => {
+    const originalUsers = [...users];
+    setUsers(users.filter(user => user.id != id));
+    axios.
+      delete<User>("https://jsonplaceholder.typicode.com/usersx/" + id).
+      then(res => console.log(res.status)).
+      catch((err) => {
+        setUsers(originalUsers);
+        setError(err);
+      });
+  }
+
+  const addUser = () => {
+    const originalUsers = [...users];
+    const newUser = { id: 0, name: "Mayank Gupta" };
+    setUsers([...users, newUser]);
+    axios.
+      post<User>("https://jsonplaceholder.typicode.com/users/", newUser).
+      then(({ data: savedUser }) => setUsers([savedUser, ...users])).
+      catch((err) => {
+        setUsers(originalUsers);
+        setError(err);
+      });
+
+  }
+
   return (
     <>
-      {error && <p>{error.code}</p>}
+      {error && <p className='text-danger mb-3'>{error.message}</p>}
       {isLoading &&
         <div className="spinner-border"></div>}
-      <ul>
-        {users.map(user => <li key={user.id}>{user.name}</li>)}
+      <button className="btn btn-primary mb-3" onClick={addUser}>Add</button>
+      <ul className="list-group">
+        {users.map(user =>
+          <li className="list-group-item d-flex justify-content-between" key={user.id}>{user.name}
+            <button
+              onClick={() => handleDelete(user.id)}
+              className="btn btn-outline-danger">
+              Delete
+            </button>
+          </li>
+        )}
       </ul>
     </>
   );
